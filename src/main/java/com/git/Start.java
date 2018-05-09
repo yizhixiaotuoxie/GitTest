32d883de7fbf01f450e463397a998e09807896d3
renb <helloworld>
2018-05-09 15:02:29 +0800

    git 指令熟悉-test

---
+++ src/main/java/com/git/Start.java
@@ -0,0 +1,16 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rb
+ * @date 2018/05/09
+ */
+@SpringBootApplication
+public class Start {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+}
